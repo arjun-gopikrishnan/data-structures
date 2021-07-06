cc97fa5f00b0e2ea4fc20745a87c84fd0694f055
Arjun <cosmicpepe2001@gmail.com>
2021-07-06 17:29:29 +0530

    level order traversal

--- trees/driver.cpp
+++ trees/driver.cpp
@@ -9,28 +9,17 @@ int main(){
     for(char i='a';i<'z';i++)
         t->insert(i);
 
-    // cout<<"In order traversal\n";
-    // t->displayInOrder();
+    cout<<"In order traversal\n";
+    t->displayInOrder();
 
-    // cout<<"\n\nPre order traversal\n";
-    // t->displayPreOrder();
+    cout<<"\n\nPre order traversal\n";
+    t->displayPreOrder();
 
-    // cout<<"\n\nPost order traversal\n";
-    // t->displayPostOrder();
-
+    cout<<"\n\nPost order traversal\n";
+    t->displayPostOrder();
+    
+    cout<<"\n\nLevel order traversal\n";
     t->levelOrder();
 
 }
 
-/*OUTPUT 
-
-In order traversal
-d b a c 
-
-Pre order traversal
-a b d c
- 
-Post order traversal
-b d c a
-
-*/
