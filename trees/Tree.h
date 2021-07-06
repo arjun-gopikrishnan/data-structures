fbfbc926b795dfe0e8b5be0d07dca7dccf4065f3
Arjun <cosmicpepe2001@gmail.com>
2021-07-06 14:53:02 +0530

    trees using templates

--- trees/Tree.h
+++ trees/Tree.h
@@ -1,48 +1,105 @@
 #ifndef TREE_H
 #define TREE_H
-#include <initializer_list>
 #include<iostream>
+#include<queue>
+#include<string.h>
+#include<string>
+using namespace std;
 
 template <typename DataType>
-class TreeNode{
+class node {
     public:
-    DataType data;
-    TreeNode *rchild;
-    TreeNode *lchild;
-    TreeNode(){
-        lchild=nullptr;
-        rchild=nullptr;
-    }
-
-    TreeNode(DataType d, TreeNode *left = nullptr,TreeNode *right = nullptr){
-        data=d;
-        lchild=left;
-        rchild=right;
-    }
-
-    ~TreeNode(){
-        delete rchild;
-        delete lchild;
-    }
+        DataType data;
+        node *left, *right;
+        node(){
+            left=nullptr;
+            right=nullptr;
+        }
+        node(DataType val){
+            data=val;
+            left=nullptr;
+            right=nullptr;
+        }
 };
+
 template <typename DataType>
 class Tree{
-    private:
-        TreeNode<DataType> *root;
+    private: 
+        node<DataType>* root;
+        void inOrder(node<DataType> *rootNode);
+        void preOrder(node<DataType> *rootNode);
+        void postOrder(node<DataType> *rootNode);
     public:
-        Tree(){
-            root=nullptr;
+        void insert(DataType data);
+        void displayInOrder(){
+            inOrder(root);
         }
-        Tree(std::initializer_list <DataType>);
-        ~Tree(){
-            delete root;
+        void displayPreOrder(){
+            preOrder(root);
         }
+        void displayPostOrder(){
+            postOrder(root);
+        }
+
 };
 
-template <typename DataType> Tree<DataType>::Tree(std::initializer_list<DataType> list){
-    for(DataType d:list){
-        std::cout<<d<<" ";
+template <typename DataType>
+void Tree<DataType>::insert(DataType data) {
+    if(!root){
+        root = new node<DataType>(data);
+        return;
     }
+    node<DataType> *temp;
+    queue<node<DataType>*>q;
+    q.push(root);
+    while(!q.empty()) 
+    {
+        temp = q.front();
+        q.pop();
+
+        if(temp->left == NULL) {
+            temp->left = new node<DataType>(data);
+            break;
+        }
+
+        else
+            q.push(temp->left);
+        
+        if(temp->right == NULL) {
+            temp->right = new node<DataType>(data);
+            break;
+        }
+
+        else
+            q.push(temp->right);
+    }
+}
+
+template <typename DataType>
+void Tree<DataType>::inOrder(node<DataType> *rootNode) {
+    if(rootNode == NULL)
+        return;
+    inOrder(rootNode->left);
+    cout << rootNode->data << " ";
+    inOrder(rootNode->right);
+}
+
+template <typename DataType>
+void Tree<DataType>::preOrder(node<DataType> *rootNode) {
+    if(rootNode == NULL)
+        return;
+    cout << rootNode->data << " ";
+    preOrder(rootNode->left);
+    preOrder(rootNode->right);
+}
+
+template <typename DataType>
+void Tree<DataType>::postOrder(node<DataType> *rootNode) {
+    if(rootNode == NULL)
+        return;
+    preOrder(rootNode->left);
+    preOrder(rootNode->right);
+    cout << rootNode->data << " ";
 }
 
 #endif
--- trees/driver.cpp
+++ trees/driver.cpp
@@ -4,7 +4,30 @@
 using namespace std;
 
 int main(){
-    Tree <int> *tree =  new Tree<int>({1,2,3,4,5,6});
-    cout<<"Driver is functional"<<endl;
-   
+    Tree<char> *t = new Tree<char>();
+    
+    for(char i='a';i<'e';i++)
+        t->insert(i);
+
+    cout<<"In order traversal\n";
+    t->displayInOrder();
+
+    cout<<"\n\nPre order traversal\n";
+    t->displayPreOrder();
+
+    cout<<"\n\nPost order traversal\n";
+    t->displayPostOrder();
 }
+
+/*OUTPUT 
+
+In order traversal
+d b a c 
+
+Pre order traversal
+a b d c
+ 
+Post order traversal
+b d c a
+
+*/
